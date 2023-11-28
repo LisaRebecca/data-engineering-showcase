8a780f42d6234c9446582fd3f1b6c6283c314a23
Lisa Schmidt <61876411+LisaRebecca@users.noreply.github.com>
2023-11-28 21:00:55 +0100

    Add Exercise2 (#17)
    * Add first ideas for exercise 2
    
    * Fix ex2 filters to also include edge cases for coordinates
    
    * Change database output location
    
    * Fix datatypes for ex2 output table

---
+++ exercises/exercise2.py
@@ -0,0 +1,49 @@
+import pandas as pd
+from sqlalchemy import create_engine, BIGINT, FLOAT, TEXT
+
+DATA_URL = "https://download-data.deutschebahn.com/static/datasets/haltestellen/D_Bahnhof_2020_alle.CSV"
+DB_FILENAME = "data/trainstops.sqlite"
+DB_TABLENAME = "trainstops"
+
+df_trainstops = pd.read_csv(DATA_URL, sep=";", decimal=",")
+print(df_trainstops.columns)
+
+# First, drop the "Status" column
+df_trainstops = df_trainstops.drop(columns=["Status"])
+
+print(df_trainstops.columns)
+print(len(df_trainstops))
+
+# Valid "Verkehr" values are "FV", "RV", "nur DPN"
+valid_verkehr = ["FV", "RV", "nur DPN"]
+df_trainstops = df_trainstops[df_trainstops['Verkehr'].isin(valid_verkehr)]
+
+# Valid "Laenge", "Breite" values are geographic coordinate system values between and including -90 and 90
+df_trainstops = df_trainstops[df_trainstops['Laenge'].between(-90, 90, inclusive='both')]
+df_trainstops = df_trainstops[df_trainstops['Breite'].between(-90, 90, inclusive='both')]
+
+# Empty cells are considered invalid
+df_trainstops = df_trainstops[df_trainstops.notnull().all(axis=1)]
+
+# Valid "IFOPT" values follow this pattern:
+# <exactly two characters>:<any amount of numbers>:<any amount of numbers><optionally another colon followed by any amount of numbers>
+df_trainstops = df_trainstops[df_trainstops['IFOPT'].str.match(r'^[a-zA-Z]{2}:\d+:\d+(\:\d+)?$')]
+print(len(df_trainstops))
+
+# Use fitting SQLite types (e.g., BIGINT, TEXT or FLOAT) for all columns
+types = {
+        "EVA_NR": BIGINT,
+        "DS100": TEXT,
+        "IFOPT": TEXT,
+        "NAME": TEXT,
+        "Verkehr": TEXT,
+        "Laenge": FLOAT,
+        "Breite": FLOAT,
+        "Betreiber_Name": TEXT,
+        "Betreiber_Nr": BIGINT
+    }
+
+engine = create_engine("sqlite:///"+DB_FILENAME)
+df_trainstops.to_sql(DB_TABLENAME, engine, if_exists="replace", index=False, dtype=types)
+engine.dispose()
+
