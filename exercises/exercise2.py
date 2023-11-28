7d7ec92c6ca0073da69d22789377221e33de86d7
Lisa Schmidt <61876411+LisaRebecca@users.noreply.github.com>
2023-11-28 21:09:58 +0100

    Refactor ex2 with methods

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -5,45 +5,49 @@ DATA_URL = "https://download-data.deutschebahn.com/static/datasets/haltestellen/
 DB_FILENAME = "data/trainstops.sqlite"
 DB_TABLENAME = "trainstops"
 
-df_trainstops = pd.read_csv(DATA_URL, sep=";", decimal=",")
-print(df_trainstops.columns)
+def load_data(url: str) -> pd.DataFrame:
+    return pd.read_csv(DATA_URL, sep=";", decimal=",")
 
-# First, drop the "Status" column
-df_trainstops = df_trainstops.drop(columns=["Status"])
+def clean_data(data: pd.DataFrame) -> pd.DataFrame:
 
-print(df_trainstops.columns)
-print(len(df_trainstops))
+    # First, drop the "Status" column
+    data = data.drop(columns=["Status"])  
 
-# Valid "Verkehr" values are "FV", "RV", "nur DPN"
-valid_verkehr = ["FV", "RV", "nur DPN"]
-df_trainstops = df_trainstops[df_trainstops['Verkehr'].isin(valid_verkehr)]
+    # Valid "Verkehr" values are "FV", "RV", "nur DPN"
+    valid_verkehr = ["FV", "RV", "nur DPN"]
+    data = data[data['Verkehr'].isin(valid_verkehr)]
 
-# Valid "Laenge", "Breite" values are geographic coordinate system values between and including -90 and 90
-df_trainstops = df_trainstops[df_trainstops['Laenge'].between(-90, 90, inclusive='both')]
-df_trainstops = df_trainstops[df_trainstops['Breite'].between(-90, 90, inclusive='both')]
+    # Valid "Laenge", "Breite" values are geographic coordinate system values between and including -90 and 90
+    data = data[data['Laenge'].between(-90, 90, inclusive='both')]
+    data = data[data['Breite'].between(-90, 90, inclusive='both')]
 
-# Empty cells are considered invalid
-df_trainstops = df_trainstops[df_trainstops.notnull().all(axis=1)]
+    # Empty cells are considered invalid
+    data = data[data.notnull().all(axis=1)]
 
-# Valid "IFOPT" values follow this pattern:
-# <exactly two characters>:<any amount of numbers>:<any amount of numbers><optionally another colon followed by any amount of numbers>
-df_trainstops = df_trainstops[df_trainstops['IFOPT'].str.match(r'^[a-zA-Z]{2}:\d+:\d+(\:\d+)?$')]
-print(len(df_trainstops))
+    # Valid "IFOPT" values follow this pattern:
+    # <exactly two characters>:<any amount of numbers>:<any amount of numbers><optionally another colon followed by any amount of numbers>
+    data = data[data['IFOPT'].str.match(r'^[a-zA-Z]{2}:\d+:\d+(\:\d+)?$')]
+    
+    return data
 
-# Use fitting SQLite types (e.g., BIGINT, TEXT or FLOAT) for all columns
-types = {
-        "EVA_NR": BIGINT,
-        "DS100": TEXT,
-        "IFOPT": TEXT,
-        "NAME": TEXT,
-        "Verkehr": TEXT,
-        "Laenge": FLOAT,
-        "Breite": FLOAT,
-        "Betreiber_Name": TEXT,
-        "Betreiber_Nr": BIGINT
-    }
+def persist_data(data: pd.DataFrame, location: str, tablename: str) -> None:
+    # Use fitting SQLite types (e.g., BIGINT, TEXT or FLOAT) for all columns
+    types = {
+            "EVA_NR": BIGINT,
+            "DS100": TEXT,
+            "IFOPT": TEXT,
+            "NAME": TEXT,
+            "Verkehr": TEXT,
+            "Laenge": FLOAT,
+            "Breite": FLOAT,
+            "Betreiber_Name": TEXT,
+            "Betreiber_Nr": BIGINT
+        }
 
-engine = create_engine("sqlite:///"+DB_FILENAME)
-df_trainstops.to_sql(DB_TABLENAME, engine, if_exists="replace", index=False, dtype=types)
-engine.dispose()
+    engine = create_engine("sqlite:///"+location)
+    data.to_sql(tablename, engine, if_exists="replace", index=False, dtype=types)
+    engine.dispose()
 
+df_trainstops = load_data(DATA_URL)
+df_trainstops = clean_data(df_trainstops)
+persist_data(df_trainstops, DB_FILENAME, DB_TABLENAME)
