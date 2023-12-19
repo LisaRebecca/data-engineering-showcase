fcc12dfa15c04ee477d48b7709ae140ba68686af
Lisa Schmidt <61876411+LisaRebecca@users.noreply.github.com>
2023-12-19 20:35:01 +0100

    Install requirements directly

--- project/tests.sh
+++ project/tests.sh
@@ -3,8 +3,9 @@
     # Validates that the output file(s) exist (but make sure the data pipeline creates them and you do not check them in)
 
 #!/bin/bash
-test -d venv || virtualenv venv
-. venv/bin/activate; pip install -Ur requirements.txt
+python3 -m pip install -r requirements.txt
+#test -d venv || virtualenv venv
+#. venv/bin/activate; pip install -Ur requirements.txt
 sh ./project/pipeline.sh
 pytest project/system_tests.py
 # alternatively: python3 ./project/system_tests.py
